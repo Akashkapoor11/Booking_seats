a494732f117675404f76e0750d31690f486a8472
Akashkapoor11 <2k22.cse.2213608@gmail.com>
2024-10-06 14:15:13 +0530

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // Add FormsModule
 
 import { AppComponent } from './app.component';
+import { SeatSelectionComponent } from './seat-selection/seat-selection.component';
+import { BookingSummaryComponent } from './booking-summary/booking-summary.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SeatSelectionComponent, BookingSummaryComponent, ConfirmationComponent],
   imports: [
     BrowserModule,
     FormsModule, // Import FormsModule for two-way binding
